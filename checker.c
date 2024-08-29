9cab099e625f0cbb2fe896794317452d17624c53
Sharath-S-T <sharath.st123@gmail.com>
2024-08-29 10:10:22 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -11,52 +11,43 @@ void printSocOutOfRange()
     printf("State of Charge out of range!\n");
 }
 
-void printChargeRateOutOfRange()
-{
+void printChargeRateOutOfRange() {
     printf("Charge Rate out of range!\n");
+}
+
+int checkValue(float value, float min, float max, void (*printErrorMessage)())
+{
+    if (value < min || value > max)
+    {
+        printErrorMessage();
+        return 0;
+    }
+    return 1;
 }
 
 int checkTemperature(float temperature)
 {
-    if(temperature < 0 || temperature > 45)
-    {
-        printTemperatureOutOfRange();
-        return 0;
-    }
-    return 1;
+    return checkValue(temperature, 0, 45, printTemperatureOutOfRange);
 }
 
 int checkSoc(float soc)
 {
-    if(soc < 20 || soc > 80)
-    {
-        printSocOutOfRange();
-        return 0;
-    }
-    return 1;
+    return checkValue(soc, 20, 80, printSocOutOfRange);
 }
 
 int checkChargeRate(float chargeRate)
 {
-  if(chargeRate > 0.8)
-  {
-    printChargeRateOutOfRange();
+    return checkValue(chargeRate, 0, 0.8, printChargeRateOutOfRange);
+}
+
+int batteryIsOk(float temperature, float soc, float chargeRate)
+{
+    return checkTemperature(temperature) & checkSoc(soc) & checkChargeRate(chargeRate);
+}
+
+int main()
+{
+    assert(batteryIsOk(25, 70, 0.7));
+    assert(!batteryIsOk(50, 85, 0));
     return 0;
-  }
-  return 1;
-}
-
-int batteryIsOk(float temperature, float soc, float chargeRate) 
-{
-    int status = 1;
-    status &= checkTemperature(temperature);
-    status &= checkSoc(soc);
-    status &= checkChargeRate(chargeRate);
-    return status;
-}
-
-int main() 
-{
-  assert(batteryIsOk(25, 70, 0.7));
-  assert(!batteryIsOk(50, 85, 0));
 }
