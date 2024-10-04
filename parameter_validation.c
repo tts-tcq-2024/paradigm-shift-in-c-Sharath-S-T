965aee721af1a371871b85ada7b4e91d280a7e52
Sharath-S-T <sharath.st123@gmail.com>
2024-10-04 16:28:48 +0530

    Update parameter_validation.c

--- parameter_validation.c
+++ parameter_validation.c
@@ -22,11 +22,20 @@ void checkBounds(float value, float min, float max, const char *errorMessage) {
     }
 }
 
-void checkWarnings(float value, float min, float max, const ParameterState *state) {
+void handleWarningLow(float value, float min, float max, const ParameterState *state) {
     if (isWarningLow(value, min, max)) {
         printMessage(state->warningLow);
     }
+}
+
+void handleWarningHigh(float value, float max, const ParameterState *state) {
     if (isWarningHigh(value, max)) {
         printMessage(state->warningHigh);
     }
 }
+
+void checkWarnings(float value, float min, float max, const ParameterState *state) {
+    handleWarningLow(value, min, max, state);
+    handleWarningHigh(value, max, state);
+}
+
