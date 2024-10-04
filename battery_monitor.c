e6184c354783f9274e9e0a73c884bd13a7aea3da
Sharath-S-T <sharath.st123@gmail.com>
2024-10-04 15:25:51 +0530

    Update battery_monitor.c

--- battery_monitor.c
+++ battery_monitor.c
@@ -5,48 +5,56 @@ void printMessage(const char *message) {
     printf("%s\n", message);
 }
 
-void checkTemperature(float temperature, ParameterState *state) {
-    setParameterState(state, "Temperature out of range!", "Warning: Approaching low temperature!", "Warning: Approaching high temperature!");
-    
-    if (temperature < TEMPERATURE_MIN || temperature > TEMPERATURE_MAX) {
-        printMessage(state->errorMessage);
-    } else if (WARN_FOR_TEMPERATURE) {
-        float warningLowThreshold = TEMPERATURE_MIN + (TOLERANCE_PERCENTAGE * TEMPERATURE_MAX);
-        float warningHighThreshold = TEMPERATURE_MAX - (TOLERANCE_PERCENTAGE * TEMPERATURE_MAX);
+static void checkBounds(float value, float min, float max, const char *errorMessage) {
+    if (value < min || value > max) {
+        printMessage(errorMessage);
+    }
+}
 
-        if (temperature <= warningLowThreshold) {
-            printMessage(state->warningLow);
-        } else if (temperature >= warningHighThreshold) {
-            printMessage(state->warningHigh);
-        }
+static void checkWarnings(float value, float min, float max, const ParameterState *state) {
+    float warningLowThreshold = min + (TOLERANCE_PERCENTAGE * max);
+    float warningHighThreshold = max - (TOLERANCE_PERCENTAGE * max);
+
+    if (value <= warningLowThreshold) {
+        printMessage(state->warningLow);
+    } else if (value >= warningHighThreshold) {
+        printMessage(state->warningHigh);
+    }
+}
+
+void checkTemperature(float temperature, ParameterState *state) {
+    setParameterState(state, "Temperature out of range!", 
+                     "Warning: Approaching low temperature!", 
+                     "Warning: Approaching high temperature!");
+    
+    checkBounds(temperature, TEMPERATURE_MIN, TEMPERATURE_MAX, state->errorMessage);
+    
+    if (WARN_FOR_TEMPERATURE) {
+        checkWarnings(temperature, TEMPERATURE_MIN, TEMPERATURE_MAX, state);
     }
 }
 
 void checkSoc(float soc, ParameterState *state) {
-    setParameterState(state, "State of Charge out of range!", "Warning: Approaching discharge!", "Warning: Approaching charge-peak!");
-
-    if (soc < SOC_MIN || soc > SOC_MAX) {
-        printMessage(state->errorMessage);
-    } else if (WARN_FOR_SOC) {
-        float warningLowThreshold = SOC_MIN + (TOLERANCE_PERCENTAGE * SOC_MAX);
-        float warningHighThreshold = SOC_MAX - (TOLERANCE_PERCENTAGE * SOC_MAX);
-
-        if (soc <= warningLowThreshold) {
-            printMessage(state->warningLow);
-        } else if (soc >= warningHighThreshold) {
-            printMessage(state->warningHigh);
-        }
+    setParameterState(state, "State of Charge out of range!", 
+                     "Warning: Approaching discharge!", 
+                     "Warning: Approaching charge-peak!");
+    
+    checkBounds(soc, SOC_MIN, SOC_MAX, state->errorMessage);
+    
+    if (WARN_FOR_SOC) {
+        checkWarnings(soc, SOC_MIN, SOC_MAX, state);
     }
 }
 
 void checkChargeRate(float chargeRate, ParameterState *state) {
-    setParameterState(state, "Charge Rate out of range!", "", "Warning: Approaching charge rate peak!");
-
-    if (chargeRate > CHARGE_RATE_MAX) {
-        printMessage(state->errorMessage);
-    } else if (WARN_FOR_CHARGE_RATE) {
+    setParameterState(state, "Charge Rate out of range!", 
+                     "", 
+                     "Warning: Approaching charge rate peak!");
+    
+    checkBounds(chargeRate, 0, CHARGE_RATE_MAX, state->errorMessage);
+    
+    if (chargeRate <= CHARGE_RATE_MAX && WARN_FOR_CHARGE_RATE) {
         float warningHighThreshold = CHARGE_RATE_MAX - (TOLERANCE_PERCENTAGE * CHARGE_RATE_MAX);
-
         if (chargeRate >= warningHighThreshold) {
             printMessage(state->warningHigh);
         }
