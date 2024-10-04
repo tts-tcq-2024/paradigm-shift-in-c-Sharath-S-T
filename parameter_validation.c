69af4cf7696a461e784da633f5fa7b6d881af3d5
Sharath-S-T <sharath.st123@gmail.com>
2024-10-04 15:53:51 +0530

    Update parameter_validation.c

--- parameter_validation.c
+++ parameter_validation.c
@@ -2,19 +2,31 @@
 #include <stdio.h>
 #include "parameter_validation.h"
 
+int isOutOfRange(float value, float min, float max) {
+    return value < min || value > max;
+}
+
+int isWarningLow(float value, float min, float max) {
+    float warningLowThreshold = min + (TOLERANCE_PERCENTAGE * max);
+    return value <= warningLowThreshold;
+}
+
+int isWarningHigh(float value, float max) {
+    float warningHighThreshold = max - (TOLERANCE_PERCENTAGE * max);
+    return value >= warningHighThreshold;
+}
+
 void checkBounds(float value, float min, float max, const char *errorMessage) {
-    if (value < min || value > max) {
+    if (isOutOfRange(value, min, max)) {
         printMessage(errorMessage);
     }
 }
 
 void checkWarnings(float value, float min, float max, const ParameterState *state) {
-    float warningLowThreshold = min + (TOLERANCE_PERCENTAGE * max);
-    float warningHighThreshold = max - (TOLERANCE_PERCENTAGE * max);
-
-    if (value <= warningLowThreshold) {
+    if (isWarningLow(value, min, max)) {
         printMessage(state->warningLow);
-    } else if (value >= warningHighThreshold) {
+    }
+    if (isWarningHigh(value, max)) {
         printMessage(state->warningHigh);
     }
-}
+}}
