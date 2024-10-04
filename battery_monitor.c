9863b651968b197ef921282492991569f3628102
Sharath-S-T <sharath.st123@gmail.com>
2024-10-04 15:48:07 +0530

    Update battery_monitor.c

--- battery_monitor.c
+++ battery_monitor.c
@@ -5,17 +5,6 @@ void printMessage(const char *message) {
     printf("%s\n", message);
 }
 
-static void checkWarnings(float value, float min, float max, const ParameterState *state) {
-    float warningLowThreshold = min + (TOLERANCE_PERCENTAGE * max);
-    float warningHighThreshold = max - (TOLERANCE_PERCENTAGE * max);
-
-    if (value <= warningLowThreshold) {
-        printMessage(state->warningLow);
-    } else if (value >= warningHighThreshold) {
-        printMessage(state->warningHigh);
-    }
-}
-
 void checkTemperature(float temperature, ParameterState *state) {
     setParameterState(state, "Temperature out of range!", 
                      "Warning: Approaching low temperature!", 
