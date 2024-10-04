07c959de61b69fab129f7fe73ec000fad2831f42
Sharath-S-T <sharath.st123@gmail.com>
2024-10-04 15:16:19 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,24 @@
+// main.c
+#include "parameter_state.h"
+#include "battery_monitor.h"
+
+int main() {
+    ParameterState temperatureState;
+    ParameterState socState;
+    ParameterState chargeRateState;
+
+    // Test values for temperature, SoC, and charge rate
+    checkTemperature(46, &temperatureState); // Should print error
+    checkTemperature(39, &temperatureState); // Should print warning
+    checkTemperature(25, &temperatureState); // Should print nothing
+
+    checkSoc(85, &socState); // Should print error
+    checkSoc(76, &socState); // Should print warning
+    checkSoc(50, &socState); // Should print nothing
+
+    checkChargeRate(0.85, &chargeRateState); // Should print error
+    checkChargeRate(0.77, &chargeRateState); // Should print warning
+    checkChargeRate(0.70, &chargeRateState); // Should print nothing
+
+    return 0;
+}
