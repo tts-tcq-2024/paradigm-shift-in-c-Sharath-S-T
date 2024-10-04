db19919d8ab39cd4d244f841381177dec9363d72
Sharath-S-T <sharath.st123@gmail.com>
2024-10-04 15:33:44 +0530

    Create parameter_validation.c

---
+++ parameter_validation.c
@@ -0,0 +1,24 @@
+// parameter_validation.c
+#include <stdio.h>
+#include "parameter_validation.h"
+
+void printMessage(const char *message) {
+    printf("%s\n", message);
+}
+
+void checkBounds(float value, float min, float max, const char *errorMessage) {
+    if (value < min || value > max) {
+        printMessage(errorMessage);
+    }
+}
+
+void checkWarnings(float value, float min, float max, const ParameterState *state) {
+    float warningLowThreshold = min + (TOLERANCE_PERCENTAGE * max);
+    float warningHighThreshold = max - (TOLERANCE_PERCENTAGE * max);
+
+    if (value <= warningLowThreshold) {
+        printMessage(state->warningLow);
+    } else if (value >= warningHighThreshold) {
+        printMessage(state->warningHigh);
+    }
+}
