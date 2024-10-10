61f113bf1c2fb1fe287bb01840f5f47cd177d340
Sharath-S-T <sharath.st123@gmail.com>
2024-10-10 16:08:28 +0530

    Update main.c

--- main.c
+++ main.c
@@ -11,10 +11,12 @@ int main() {
     checkTemperature(46, &temperatureState); // Should print error
     checkTemperature(39, &temperatureState); // Should print warning
     checkTemperature(25, &temperatureState); // Should print nothing
+    checkTemperature(0, &temperatureState); // Should print warning
 
     checkSoc(85, &socState); // Should print error
     checkSoc(76, &socState); // Should print warning
     checkSoc(50, &socState); // Should print nothing
+    checkSoc(20, &socState); // Should print warning
 
     checkChargeRate(0.85, &chargeRateState); // Should print error
     checkChargeRate(0.77, &chargeRateState); // Should print warning
