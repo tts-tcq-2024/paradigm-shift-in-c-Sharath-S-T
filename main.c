fd692fc01199362f13ad29d79c3f64da1454bc4c
Sharath-S-T <sharath.st123@gmail.com>
2024-10-16 19:46:59 +0530

    Update main.c

--- main.c
+++ main.c
@@ -3,20 +3,12 @@
 #include "batteryValidation.h"
 
 int main() {
-    // // Test cases for battery validation
-    // assert(batteryIsOk(25, 70, 0.7)); // Normal values
-    // assert(!batteryIsOk(50, 85, 0));   // Out of range values
-    // assert(batteryIsOk(44, 79, 0.79)); // Near upper limits
-    // assert(batteryIsOk(6, 21, 0.05));     // Near lower limits
-
-    // // Additional test cases
-    // assert(batteryIsOk(30, 22, 0.5));  // Valid case
-    // assert(!batteryIsOk(46, 30, 0.2));  // Temperature out of range
-    // assert(!batteryIsOk(20, 18, 0.9));  // SOC and Charge Rate out of range
-    assert(batteryIsOk(25, 70, 0.7));  
-    assert(!batteryIsOk(50, 85, 0.9));  
-    assert(batteryIsOk(43, 77, 0.78));  
-    assert(batteryIsOk(2, 22, 0));  
+    assert(batteryIsOk(25, 70, 0.7));   // Valid case
+    assert(!batteryIsOk(50, 85, 0.9));  // All Invalid Case
+    assert(batteryIsOk(43, 77, 0.78));  // Near Upper limit case
+    assert(batteryIsOk(2, 22, 0));      // Near Lower limit case
+    assert(!batteryIsOk(46, 30, 0.2));  // Temperature out of range
+    assert(!batteryIsOk(20, 18, 0.95));  // SOC and Charge Rate out of range
 
     printf("All test cases passed!\n");
     return 0;
