15a962bd866ab5e2a8431e50769ff8372c068ffe
Sharath-S-T <sharath.st123@gmail.com>
2024-10-16 12:45:45 +0530

    Update batteryValidation.c

--- batteryValidation.c
+++ batteryValidation.c
@@ -2,19 +2,18 @@
 #include "batteryValidation.h"
 #include "utility.h"
 
-int checkRange(float value, float min, float max) {
-    return value >= min && value <= max;
-}
+// Check status function
+int checkStatus(float value, const BatteryParameter* config)
+{
+    #if config->enableWarnings
+        handleUpperLimit(config, value);
+        handleLowerLimit(config, value); 
+    #endif
 
-int checkStatus(float value, float min, float max, float warningTolerance, const char *errorMessage, const char *warningMessage) {
-    if (!checkRange(value, min, max)) {
-        printf("%s (Value: %.2f, Range: [%.2f, %.2f])\n", errorMessage, value, min, max);
-        return 0; // Out of range
+    if (handleOutOfRange(config, value)) {
+        return 0; 
     }
-    if ((value >= (max - warningTolerance)) || (value <= (min + warningTolerance))) {
-        printWarning(value, warningMessage);
-    }
-    return 1; // Within range
+    return 1;
 }
 
 int batteryIsOk(float temperature, float soc, float chargeRate) {
