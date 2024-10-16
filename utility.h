ecdef9626cdcff7679ec136efc6de2a033fd5ab0
Sharath-S-T <sharath.st123@gmail.com>
2024-10-16 12:34:16 +0530

    Update utility.h

--- utility.h
+++ utility.h
@@ -1,6 +1,22 @@
 #ifndef UTILITY_H
 #define UTILITY_H
 
+// Structure to hold battery parameters
+typedef struct {
+    float min;
+    float max;
+    float warningTolerance;
+    int enableWarnings;
+    const char* outOfRangeMessage;
+    const char* warningLowerMessage;
+    const char* warningUpperMessage;
+} BatteryParameter;
+
+// Function declarations
+extern const BatteryParameter TEMP_CONFIG;
+extern const BatteryParameter SOC_CONFIG;
+extern const BatteryParameter CHARGE_RATE_CONFIG;
+
 void printWarning(float value, const char *warningMessage);
 
 #endif // UTILITY_H
