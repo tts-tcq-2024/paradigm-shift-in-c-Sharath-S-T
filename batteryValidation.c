de3de415ee25f63c26c115de57cf1159b0bee44f
Sharath-S-T <sharath.st123@gmail.com>
2024-10-28 15:33:40 +0530

    Update batteryValidation.c

--- batteryValidation.c
+++ batteryValidation.c
@@ -3,21 +3,21 @@
 #include "utility.h"
 
 // Check status function
-int checkStatus(float value, const BatteryParameter* config, BatteryParameterIndex index)
+int checkStatus(float value, const BatteryParameter* config, BatteryParameterIndex index, BatteryStatus* status)
 {
     if (config->enableWarnings)
     {
-        handleUpperLimit(config, index, value);
-        handleLowerLimit(config, index, value); 
+        handleUpperLimit(config, status, index, value);
+        handleLowerLimit(config, status, index, value); 
     }
 
-    if (handleOutOfRange(config, index, value)) {
+    if (handleOutOfRange(config, status, index, value)) {
         return 0; 
     }
     return 1;
 }
 
-int batteryIsOk(float temperature, float soc, float chargeRate) {
+int batteryIsOk(float temperature, float soc, float chargeRate, BatteryStatus* status) {
     int tempStatus = checkStatus(temperature, &TEMP_CONFIG, PARAM_TEMP);
     int socStatus = checkStatus(soc, &SOC_CONFIG, PARAM_SOC);
     int chargeRateStatus = checkStatus(chargeRate, &CHARGE_RATE_CONFIG, PARAM_CHARGE_RATE);
