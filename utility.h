615d3360c1771e85066f9a9cbeba612d32df7c09
Sharath-S-T <sharath.st123@gmail.com>
2024-10-28 11:58:33 +0530

    Update utility.h

--- utility.h
+++ utility.h
@@ -12,19 +12,24 @@ typedef struct {
     const char* warningUpperMessage;
 } BatteryParameter;
 
-#define MAX_PARAMETERS 3
-
 typedef struct {
-    float value[MAX_PARAMETERS];              // Stores temperature, SOC, charge rate
-    const char* outOfRangeMessage[MAX_PARAMETERS]; // Out of range messages
-    const char* warningLowerMessage[MAX_PARAMETERS]; // Lower limit warning messages
-    const char* warningUpperMessage[MAX_PARAMETERS]; // Upper limit warning messages
+    float value[PARAM_COUNT];              // Stores temperature, SOC, charge rate
+    const char* outOfRangeMessage[PARAM_COUNT]; // Out of range messages
+    const char* warningLowerMessage[PARAM_COUNT]; // Lower limit warning messages
+    const char* warningUpperMessage[PARAM_COUNT]; // Upper limit warning messages
 } BatteryStatus
 
 // Function declarations
 extern const BatteryParameter TEMP_CONFIG;
 extern const BatteryParameter SOC_CONFIG;
 extern const BatteryParameter CHARGE_RATE_CONFIG;
+
+typedef enum {
+    PARAM_TEMP,
+    PARAM_SOC,
+    PARAM_CHARGE_RATE,
+    PARAM_COUNT // This will give the count of parameters
+} BatteryParameterIndex;
 
 void printErrorMessage(const char *warningMessage, float value);
 int handleLowerLimit(const BatteryParameter* config, float value);
